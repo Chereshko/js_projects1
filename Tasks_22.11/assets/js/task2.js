9452aa223b944c448eda80bcf2bccb3a2a4bfb0a
Vasy Terkin <vasy.terkin12@gmail.com>
2019-11-24 13:26:17 +0200

    Add: review of task2

--- Tasks_22.11/assets/js/task2.js
+++ Tasks_22.11/assets/js/task2.js
@@ -1,61 +1,115 @@
-'use strict'
+'use strict';
 
 let arr1 = [];
-for(let i= 0; i<20; i++){
-    arr1[i] = Math.floor(Math.random()*100);
+for (let i = 0; i < 20; i++) {
+    arr1[i] = Math.floor(Math.random() * 100);
 }
 
+/*
+* 1. Функция принимает массив и выводит его на экран.
+* */
 
-function print(arr){
-    console.log(arr);
+//имя функции должно описывать ее работу. Контекста может и не быть. print на принтере или где? печать чего? logArray или даже logArrayElems
+function print(arr) {
+    //console.log(arr);
+    if (Array.isArray(arr)) {
+        arr.forEach(
+            currentElem => {
+
+                console.log(currentElem);
+
+            }
+        )
+    }
 }
 
-print(arr1);
+//print(arr1);
 
-function findEven(arr){
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]%2 === 0){
+function findEven(arr)/*logEvenArrayElems*/ {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
             console.log(arr[i]);
         }
     }
+
+    /* if (Array.isArray(arr)) {
+         arr.forEach(
+             currentElem => {
+                 if (currentElem % 2 === 0) {
+
+                     console.log(currentElem);
+
+                 }
+             }
+         )
+     }*/
+
 }
 
-findEven(arr1);
+//findEven(arr1);
+/*
+    3. Функция принимает массив и возвращает сумму всех элементов массива.
+*/
+function getSumOfArrayElems(arr) {
 
-function sum(arr){
-    let sum = 0;
-    for(let i=0; i<arr.length; i++){
-        sum += arr[i];
+    if (Array.isArray(arr)) {
+        let sum = 0;
+        arr.forEach(
+            (currentElem) => {
+
+                sum += currentElem;
+
+            }
+        );
+        return sum;
     }
-    console.log(sum);
+
+
+    /*for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }*/
+
+
+//    console.log(sum);
+
+
 }
 
-sum(arr1);
+//sum(arr1);
 
-function maxElement(arr){
-    let max= arr[0];
-    for(let i=1; i<arr.length; i++){
-        if(max < arr[i]){
+function maxElement(arr) {
+
+    let max = arr[0];
+    // arr.forEach =)
+    for (let i = 1; i < arr.length; i++) {
+        if (max < arr[i]) {
             max = arr[i];
         }
 
     }
-    console.log(max);
+   // console.log(max);
+    return max;
 }
 
-maxElement(arr1);
+//maxElement(arr1);
 
-function addElement(arr, element, index){
+//привычный порядок elem, index, arr
+
+function addElement(arr, element, index) {
     arr.splice(index, 0, element);
     console.log(arr);
+    return arr;
 }
 
-addElement(arr1, 444, 5);
+//addElement(arr1, 444, 5);
 
-function minusElement(arr, index){
+function minusElement(arr, index) {
     arr.splice(index, 1);
     console.log(arr);
+    return arr;
 }
-minusElement(arr1, 4);
+
+//minusElement(arr1, 4);
 
 
